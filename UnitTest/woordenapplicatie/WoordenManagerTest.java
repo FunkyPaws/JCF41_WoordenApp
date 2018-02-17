98c3ddb11ae3a634bac1631e99ec8ce58b0cbdfb
Genise-PC <springtouw@hotmail.com>
2018-02-17 18:51:07 +0100

    third unittest complete

--- UnitTest/woordenapplicatie/WoordenManagerTest.java
+++ UnitTest/woordenapplicatie/WoordenManagerTest.java
@@ -10,20 +10,21 @@ public class WoordenManagerTest {
     private WoordenManager manager;
     private String testString1;
     private String testString2;
-
+    private String testString3;
 
     @Before
     public void setUp() throws Exception {
         manager = new WoordenManager();
         testString1 = "This is the test string, randomly. allocated characters and words";
         testString2 = "this is a test string";
+        testString3 = "This is a test, a test this is";
     }
 
     @Test
     public void splitString() throws Exception {
         String[] result1 = manager.splitString(testString2);
         String[] result2 = testString2.split(" ");
-        
+
         String[] result3 = manager.splitString(testString1);
         String[] result4 = testString1.split("[,\n .]+");
 
@@ -54,6 +55,21 @@ public class WoordenManagerTest {
 
     @Test
     public void getAmountDistinctWords() throws Exception {
+        int amount1 = 5;
+        int amount2 = 4;
+
+        String[] test1 = manager.splitString(testString2);
+        String[] test2 = manager.splitString(testString3);
+
+        int result1 = manager.getAmountDistinctWords(test1);
+        int result2 = manager.getAmountDistinctWords(test2);
+
+        assertEquals(amount1, result1);
+        assertEquals(amount2, result2);
+
+        assertNotEquals(amount1, result2);
+        assertNotEquals(amount2, result1);
+
     }
 
     @Test
