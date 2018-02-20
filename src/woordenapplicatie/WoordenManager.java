4cdb35b9e6a915bda809b7882c8deae27ab5e3a0
GeniseTop2 <springtouw@hotmail.com>
2018-02-20 12:44:28 +0100

    added time checker for methodes in woordenmanager

--- src/woordenapplicatie/WoordenManager.java
+++ src/woordenapplicatie/WoordenManager.java
@@ -1,27 +1,56 @@
 package woordenapplicatie;
 
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WoordenManager {
+    private static final Logger logger = Logger.getLogger(WoordenManager.class.getName());
 
     public WoordenManager() {
     }
 
     public String[] splitString(String string) {
-        return string.toLowerCase().split("[,\n .]+");
+        long startTime = System.nanoTime();
+
+        String[] returnString = string.toLowerCase().split("[,\n .]+");
+
+        long resultTime = System.nanoTime() - startTime;
+        String logMessage = String.format("getSplitText - Time measured: %d nanoseconds", resultTime);
+        logger.log(Level.INFO, logMessage);
+
+        return returnString;
     }
 
     public Integer getAmountAllWords(String[] strings) {
-        return strings.length;
+        long startTime = System.nanoTime();
+
+        Integer returnInt = strings.length;
+
+        long resultTime = System.nanoTime() - startTime;
+        String logMessage = String.format("getSplitText - Time measured: %d nanoseconds", resultTime);
+        logger.log(Level.INFO, logMessage);
+
+        return returnInt;
     }
 
-    public Integer getAmountDistinctWords(String[] strings){
+    public Integer getAmountDistinctWords(String[] strings) {
+        long startTime = System.nanoTime();
+
         Set<String> woorden = new TreeSet<>();
         Collections.addAll(woorden, strings);
-        return woorden.size();
+        Integer returnInt = woorden.size();
+
+        long resultTime = System.nanoTime() - startTime;
+        String logMessage = String.format("getSplitText - Time measured: %d nanoseconds", resultTime);
+        logger.log(Level.INFO, logMessage);
+
+        return returnInt;
     }
 
     public Map<String, Integer> getFrequency(String[] strings) {
+        long startTime = System.nanoTime();
+
         HashMap<String, Integer> frequency = new LinkedHashMap<>();
         for (String string : strings) {
             if (frequency.containsKey(string)) {
@@ -30,25 +59,37 @@ public class WoordenManager {
                 frequency.put(string, 1);
             }
         }
+
+        long resultTime = System.nanoTime() - startTime;
+        String logMessage = String.format("getSplitText - Time measured: %d nanoseconds", resultTime);
+        logger.log(Level.INFO, logMessage);
+
         return frequency;
     }
 
     public Map<String, Set<Integer>> getConcordatie(String string) {
+        long startTime = System.nanoTime();
+
         TreeMap<String, Set<Integer>> concordatie = new TreeMap<>();
         int lines = 0;
         for (String line : string.toLowerCase().split("[\n]+")) {
             lines++;
-            for(String word: line.split("[,\n .]+")){
-                if(word.isEmpty()){
+            for (String word : line.split("[,\n .]+")) {
+                if (word.isEmpty()) {
                     continue;
                 }
-                if(!concordatie.containsKey(word)){
+                if (!concordatie.containsKey(word)) {
                     concordatie.put(word, new HashSet<>());
                 }
-                Set<Integer>set = concordatie.get(word);
+                Set<Integer> set = concordatie.get(word);
                 set.add(lines);
             }
         }
+
+        long resultTime = System.nanoTime() - startTime;
+        String logMessage = String.format("getSplitText - Time measured: %d nanoseconds", resultTime);
+        logger.log(Level.INFO, logMessage);
+
         return concordatie;
     }
 }
